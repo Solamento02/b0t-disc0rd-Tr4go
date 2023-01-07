93f69346d17146e7d177430351393c8dfa6368db
Noah Vogel <103073069+Solamento02@users.noreply.github.com>
2023-01-07 00:51:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const Discord = require('discord.js');
-const Client = require('./client/client');
-const { REST} = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const config = require('./config.json');
 const fs = require('fs');
-const { fileURLToPath } = require('url');
-const client = new Client();
-const commands = [];
+const Discord = require('discord.js');
+const Client = require('./client/Client.js');
+const config = require('./config.json');
 const prefix = "$";
-client.commands = new Discord.Collection();
-const rest = new REST({version:'9'}).setToken(config.token);
 
-const clientId = "client do seu bot";
-const guildId = "guild do seu server";
+const client = new Client();
+const clientId = "seu clientId";
+const guildId = "seu guildId";
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-  }
 
 client.on("ready", () => {
     console.log(`Bot iniciado, com ${client.users.cache.size} usuários ativos.. em ${client.channels.cache.size} canais, em ${client.guilds.cache.size} servidores..🚬 `);
@@ -29,28 +17,24 @@ client.on("ready", () => {
 client.on("ready", () => { 
     console.log(`Hey! Logado como ${client.user.tag} 🚬`);
 });
-  
-client.on('interactionCreate', async interaction => {
- const command = client.commands.get(interaction.commandName.toLowerCase());
-  
-   try {
-    if (interaction.commandName == 'userinfo') {
-       command.execute(interaction, client);
-      }
-    } catch (error) {
-      console.error(error);
-      interaction.followUp({
-        content: 'Opa! Deu erro😳',
-      });
-    }
-  });
 
 client.on('messageCreate', async (msg) => {
-    if(msg.content === prefix + 'Hey') {
+    if(msg.content === prefix + 'Boiola') {
         await msg.channel.send('Opa.. ' + msg.author.username + ' Como vai? 🚬');
     }
 });
 
+client.on('messageCreate', async (msg) => {
+    if(msg.content === prefix + 'ping'){
+         await msg.channel.send(`Pong! 🏓 ${Math.round(client.ws.ping)} ms `)
+    }
+});
 
-console.log(client.commands);
+client.on('messageCreate', async (msg) => {
+    if(msg.content === prefix + 'teste') {
+        await msg.channel.send('teste');
+    }
+});
+
+
 client.login(config.token);
